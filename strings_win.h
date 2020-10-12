9ea5931d266bab3a930ea7c7a08f5f37b5af9b44
Samuele Stasi <samuele.stasi2002@gmail.com>
2020-10-12 11:54:47 +0200

    Removed 'using std::string' and added a file to store resources

--- strings_win.h
+++ strings_win.h
@@ -2,11 +2,9 @@
 #include <string>
 #include "SfmlAPI.hpp"
 
-using std::string;
-
-static string icon_img = "C:/Users/" + SfmlAPI::getSystemUser() + "/AppData/Local/Snake/img/icon.bmp";
-static string acc_confirm_file = "C:/Users/" + SfmlAPI::getSystemUser() + "/AppData/Local/Snake/src/firstAccess.confirm";
-static string head_dir = "C:/Users/" + SfmlAPI::getSystemUser() + "/AppData/Local/Snake/img/heads";
-static string font_arial_file = "Font/arial.ttf";
-static string points_file = "C:/Users/" + SfmlAPI::getSystemUser() + "/AppData/Local/Snake/src/points";
-static string username_file = "C:/Users/" + SfmlAPI::getSystemUser() + "/AppData/Local/Snake/src/username";
+static std::string icon_img = "C:/Users/" + SfmlAPI::getSystemUser() + "/AppData/Local/Snake/img/icon.bmp";
+static std::string acc_confirm_file = "C:/Users/" + SfmlAPI::getSystemUser() + "/AppData/Local/Snake/src/firstAccess.confirm";
+static std::string head_dir = "C:/Users/" + SfmlAPI::getSystemUser() + "/AppData/Local/Snake/img/heads/";
+static std::string font_arial_file = "Font/arial.ttf";
+static std::string points_file = "C:/Users/" + SfmlAPI::getSystemUser() + "/AppData/Local/Snake/src/points";
+static std::string username_file = "C:/Users/" + SfmlAPI::getSystemUser() + "/AppData/Local/Snake/src/username";
